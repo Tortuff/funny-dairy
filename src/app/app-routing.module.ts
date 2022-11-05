4b998253ad8556d656341dc9dddbe1097024166d
Jevgenij Bielykh <j.bielykh@adus.tech>
2022-11-05 19:34:35 +0100

    Base href settings.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { LoginComponent } from '@public/pages/login/login.component';
 
-const routes: Routes = [{ path: '', component: LoginComponent }];
+const routes: Routes = [
+  {
+    path: '/fanny-dairy/fanny-dairy',
+    component: AppComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { APP_BASE_HREF } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [{ provide: APP_BASE_HREF, useValue: location.pathname }],
 })
-export class AppComponent {
-  constructor() {
-    console.log('[LOCATION] ==> ', location.pathname);
-  }
-}
+export class AppComponent {}
