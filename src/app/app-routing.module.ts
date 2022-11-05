dce36db20cdd7f6bd511beb7eae09590517d4356
Jevgenij Bielykh <j.bielykh@adus.tech>
2022-11-05 19:56:48 +0100

    Base href settings.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppLoginComponent } from '@public/pages/login/app-login.component';
 
 const routes: Routes = [
+  { path: 'funny-dairy', component: AppLoginComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: AppLoginComponent },
   { path: '**', redirectTo: '/notfound' },
 ];
 
