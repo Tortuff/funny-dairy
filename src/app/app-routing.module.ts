05947ac7b49302a628cf766c11cfb7c66dbe0e1b
Jevgenij Bielykh <j.bielykh@adus.tech>
2022-11-05 20:33:03 +0100

    Base href settings.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppLoginComponent } from '@public/pages/login/app-login.component';
+import { UserConfigurationsComponent } from '@private/pages/user-configurations/user-configurations.component';
 
 const routes: Routes = [
   { path: 'funny-dairy', component: AppLoginComponent },
+  { path: '', component: UserConfigurationsComponent },
   // { path: '**', redirectTo: 'funny-dairy' },
 ];
 
--- src/app/private/private.module.ts
+++ src/app/private/private.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserConfigurationsComponent } from './pages/user-configurations/user-configurations.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserConfigurationsComponent
+  ],
   imports: [CommonModule],
 })
 export class PrivateModule {}
