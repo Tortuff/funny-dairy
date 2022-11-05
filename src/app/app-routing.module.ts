34eb58149dd6ca7902bcd707e2d9e0f5672f3e98
Jevgenij Bielykh <j.bielykh@adus.tech>
2022-11-05 19:43:04 +0100

    Base href settings.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { LoginComponent } from '@public/pages/login/login.component';
 
 const routes: Routes = [
   {
     path: '/fanny-dairy/fanny-dairy',
-    component: AppComponent,
-    children: [
-      {
-        path: '',
-        component: LoginComponent,
-      },
-    ],
+    component: LoginComponent,
   },
 ];
 
