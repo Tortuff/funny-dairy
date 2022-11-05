a470826a0d3fa9794325b25a3b3c4207b5a68297
Jevgenij Bielykh <j.bielykh@adus.tech>
2022-11-05 19:30:03 +0100

    Base href settings.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [{ provide: APP_BASE_HREF, useValue: location.pathname }],
 })
 export class AppComponent {
   constructor() {
-    console.log('[LOCATION] ==> ', location);
+    console.log('[LOCATION] ==> ', location.pathname);
   }
 }
